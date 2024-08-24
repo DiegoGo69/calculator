19bb5ade7bbd9e2256c340e0a87fbf921212765d
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2024-08-23 21:04:14 -0400

    FEAT: ADD copy, power, clear, delete, float-point
    Copy to clipboard, get the power of a number
    clear the display content, delete the last character enter
    and perform floating point arithmetic operation

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // num ope num match /g finds all matches
 //let regex = /^(\d) ?([+-*\/]) ?(\d) ?[+-*\/=]/;
-let regex = /^(\d+) ?(\+|\-|\*|\/) ?(\d+) ?(?:\+|\-|\*|\/|=)/;
+// let regex = /^(\d+) ?(\+|\-|\*|\/) ?(\d+) ?(?:\+|\-|\*|\/|=)/;
+let regex = /^(\d+\.?\d*) ?([+\-*\/\^]) ?(\d+\.?\d*) ?(?:[+\\*\/\^=])/;
+
 let displayContent = "";
 
 const screenMain = document.querySelector('.screenMain');
@@ -88,19 +90,20 @@ keyboard.addEventListener('click', event => {
 
 
         case "c":
-            displayContent += 'CLEAR';
+            displayContent = '';
             display(displayContent);
             break;
         case "del":
-            displayContent += "DELETE";
+            displayContent = displayContent.split("").slice(0, (displayContent.length - 1)).join("");
             display(displayContent);
             break;
         case "pow":
-            displayContent += "POWER";
+            displayContent += "^";
             display(displayContent);
             break;
         case "cp":
-            displayContent += "COPY";
+            copyToClipboard();
+
             display(displayContent);
             break;
         // DEFAULT ???
@@ -125,6 +128,7 @@ function add(a, b) {return a + b}
 function substract(a, b) {return a - b}
 function multiply(a, b) {return a * b}
 function divide(a, b) {return a / b}
+function power(a, b) {return a ** b}
 
 function operate(a, b, operator) {
     a = +a;
@@ -143,9 +147,28 @@ function operate(a, b, operator) {
         case '/':
             result = divide(a, b);
             break;    
+        case '^':
+            result = power(a, b);
+            break; 
     }
     return result;
 }
+
+function copyToClipboard() {
+      // Select the text field
+  const copy = document.querySelector('#clipBoard');
+  copy.value = displayContent;
+  copy.select();
+  copy.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copy.value);
+
+  // Alert the copied text
+  console.log("Copied the text: " + copy.value);
+}
+
+// if operator follow by an operator invalid
 
 /* I could use an input textbox, and add an event listener on change
 so if the user enters input it works,
